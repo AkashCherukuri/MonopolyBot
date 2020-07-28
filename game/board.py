646c87b763a8ceee438673332351d17bca7dc4e2
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-07-28 21:25:57 +0530

    Initial Commit
    Basic board data entry done.

---
+++ game/board.py
@@ -0,0 +1,10 @@
+class Cell:
+    def __init__(self):
+        self.cell = {"id": None, "color": None, "cost": None, "rent": None, "hsc": None, "h1r": None, "h2r": None, "h3r": None, "h4r": None, "htr": None}
+
+class Board:
+    def __init__(self):
+        self.board = []
+
+    def init(self):
+        pass
---
+++ monopoly.py
@@ -0,0 +1,3 @@
+from game import *
+
+print("This is the main module to be run")
