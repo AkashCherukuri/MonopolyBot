89e1643fe8c5aa211a717be0eb8823cad1ec4639
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-08-01 14:24:07 +0530

    Update to include prompter and increment functions

--- game/utils.py
+++ game/utils.py
@@ -4,3 +4,12 @@ def roll():
     die1 = randint(1, 6)
     die2 = randint(1, 6)
     return (die1 + die2)
+
+def prompt(str = ""):
+    print(str)
+    inp = input("Monpoly>> ")
+    print()
+    return inp
+
+def increment(init_pos, roll):
+    return ((init_pos + roll) % 40)
