5da13650b41251da0325b52aa5415b232b925280
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-08-01 22:29:50 +0530

    Update to cycle through players

--- game/game.py
+++ game/game.py
@@ -16,12 +16,16 @@ class Game:
         self.board.init()
         self.state = State.GAME_LOBBY
         print(f"Game of Monopoly has been initialized. Please follow the instructions on screen and enter your inputs accordingly.\n")
-
+    
     def add_player(self):
         new = Player()
         new.init(prompt(f"Enter alias for Player{len(self.players) + 1}"))
         self.players.append(new)
+
+    def advance_turn(self):
+        self.turn = next(self.turn_cycler)
     
+    #All game logic in here
     def evaluate(self):
         if self.state == State.GAME_LOBBY:
             while True:
@@ -32,5 +36,12 @@ class Game:
                     break
                 else:
                     print(f"Only 2 to 8 players are supported as of now, please try again.\n")
+
+            self.turn_cycler = cycle(self.players)
+            self.advance_turn()
+
             self.state = State.GAME_BEGIN
             self.evaluate()
+        
+        elif self.state == State.GAME_BEGIN:
+            pass
