0c7cc5181b3bf9f0ae5dffee3e8e72502606b777
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-08-01 14:23:35 +0530

    Update to initialize players and board

--- game/game.py
+++ game/game.py
@@ -1,12 +1,36 @@
 from itertools import cycle
 from board import Board
-from utils import roll
+from utils import *
 from const import State
+from player import Player
 
 class Game:
     def __init__(self):
+        self.state = None
         self.players = []
         self.turn_cycler = None
         self.turn = None
-    def init():
-        pass
+
+    def init(self):
+        self.board = Board()
+        self.board.init()
+        self.state = State.GAME_LOBBY
+        print(f"Game of Monopoly has been initialized. Please follow the instructions on screen and enter your inputs accordingly.\n")
+
+    def add_player(self):
+        new = Player()
+        new.init(prompt(f"Enter alias for Player{len(self.players) + 1}"))
+        self.players.append(new)
+    
+    def evaluate(self):
+        if self.state == State.GAME_LOBBY:
+            while True:
+                num_players = int(prompt(f"Please enter the number of players."))
+                if num_players < 9 and num_players > 1:
+                    for i in range(num_players):
+                        self.add_player()
+                    break
+                else:
+                    print(f"Only 2 to 8 players are supported as of now, please try again.\n")
+            self.state = State.GAME_BEGIN
+            self.evaluate()
