24e68a48e068498028279f6139d4c0f7d0c6fb9b
Akash Cherukuri <akashcherukuri007@gmail.com>
2020-08-02 14:25:09 +0530

    Change game states to better suit gameplay

--- game/const.py
+++ game/const.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class State(Enum):
     GAME_BEGIN = 1 #Initialising Game, distribute money and stuff
-    GAME_CONT = 2 #Game being played by humans/bots
-    GAME_END = 3 #End the game if anyone person has been bankrupted or all humans decide to end game
+    TURN_BEGIN = 2 #Turn of a player/bot begins
+    TURN_END = 3 #End a player's turn, and ask everyone if they wanna build houses somewhere
+    GAME_END = 4 #End the game if anyone person has been bankrupted or all humans decide to end game
         
